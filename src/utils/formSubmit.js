092984a17ca52edfb036f570540ca9021fa9bcc2
tatd3v <tatdev91@gmail.com>
2026-08-18 10:05:59 -0500

    refactor: change form submission Content-Type from application/json to text/plain for no-cors compatibility

--- src/utils/formSubmit.js
+++ src/utils/formSubmit.js
@@ -27,7 +27,7 @@ export async function submitForm(formData) {
       method: 'POST',
       mode: 'no-cors',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/plain',
       },
       body: JSON.stringify(payload)
     })
