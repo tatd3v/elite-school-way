1c118a88502ed105f6d66cdbba1113a47229f334
tatd3v <tatdev91@gmail.com>
2026-08-23 05:50:44 -0500

    refactor: replace React plugin with Preact and increase test timeouts in vitest config
    Replace @vitejs/plugin-react with @preact/preset-vite; add testTimeout of 30000ms and hookTimeout of 60000ms to accommodate longer-running tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
     css: true,
+    testTimeout: 30000,
+    hookTimeout: 60000,
   },
 })
